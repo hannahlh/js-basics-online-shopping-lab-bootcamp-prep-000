626152c6a1857cf5793bdec9d8ea3a75c36ce0b4
Hannah Hughes <hugheshannah96@gmail.com>
2018-06-04 17:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,3 +38,7 @@ function generateCartItem(itemName) {
     itemPrice:getRandomInt(1, 100)
   }
 }
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
